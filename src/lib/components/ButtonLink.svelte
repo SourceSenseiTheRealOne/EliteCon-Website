ec8d66f93a8e7c1e78261b571c9dcdedcfd5075e
alexeitolkunov <alexei.tolkunov@rneit.com>
2025-02-07 10:52:11 +0000

    quick fix colour scheme banner

--- src/lib/components/ButtonLink.svelte
+++ src/lib/components/ButtonLink.svelte
@@ -13,7 +13,7 @@
 	{...$$restProps}
 	on:click
 	class={clsx(
-		'relative inline-flex h-fit w-fit rounded-xl border border-orange-100/20 bg-orange-300/10 px-4 py-2 text-orange-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-orange-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-orange-200/40 hover:text-orange-300 after:hover:bg-opacity-15 focus:ring-2',
+		'relative inline-flex h-fit w-fit rounded-xl border border-orange-100/20 bg-orange-300/10 px-4 py-2 text-orange-200 outline-none ring-orange-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-orange-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-orange-200/40 hover:text-orange-300 after:hover:bg-opacity-15 focus:ring-2',
 		className
 	)}
 >
